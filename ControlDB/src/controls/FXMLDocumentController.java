a67a868d1d8c9efc0d7764c382d71aa0445f20fa
maxhochleitner <max.hochleitner@gmail.com>
2023-06-02 08:37:25 +0200

    Update 2.06 Erste Stunde

--- ControlDB/src/controls/FXMLDocumentController.java
+++ ControlDB/src/controls/FXMLDocumentController.java
@@ -1,82 +1,105 @@
-package controls;
+    package controls;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.beans.binding.BooleanBinding;
-import javafx.beans.property.ListProperty;
-import javafx.beans.property.ReadOnlyIntegerWrapper;
-import javafx.beans.property.SimpleListProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
-import model.Person;
-import model.PersonDAO;
+    import java.net.URL;
+    import java.util.ResourceBundle;
+    import javafx.beans.binding.BooleanBinding;
+    import javafx.beans.property.ListProperty;
+    import javafx.beans.property.ReadOnlyIntegerWrapper;
+    import javafx.beans.property.SimpleListProperty;
+    import javafx.collections.FXCollections;
+    import javafx.collections.ObservableList;
+    import javafx.event.ActionEvent;
+    import javafx.fxml.FXML;
+    import javafx.fxml.Initializable;
+    import javafx.scene.control.Button;
+    import javafx.scene.control.TextField;
+    import javafx.scene.text.Text;
+    import model.Person;
+    import model.PersonDAO;
 
-public class FXMLDocumentController implements Initializable {
-    private Person actPerson;
-    private ObservableList<Person> myPersonList;
-    private ListProperty<Person> listProperty = new SimpleListProperty<>();
-    private ReadOnlyIntegerWrapper currentIndex = new ReadOnlyIntegerWrapper(-1);
+    public class FXMLDocumentController implements Initializable {
+        private Person actPerson;
+        private ObservableList<Person> myPersonList;
+        private ListProperty<Person> listProperty = new SimpleListProperty<>();
+        private ReadOnlyIntegerWrapper currentIndex = new ReadOnlyIntegerWrapper(-1);
 
-    @FXML
-    private TextField firstname;
-    @FXML
-    private TextField lastname;
-    @FXML
-    private TextField gebdat;
-    @FXML
-    private TextField email;
-    @FXML
-    private TextField position;
-    @FXML
-    private TextField marktvalue;
-    @FXML
-    private TextField mannschaftid;
-    @FXML
-    private Button confirmbtn;
-    @FXML
-    private Button showbtn;
-    @FXML
-    private Button forwardbtn;
-    @FXML
-    private Button backbtn;
-    @FXML
-    private Button deletebtn;
-    @FXML
-    private TextField natio;
+        @FXML
+        private TextField firstname;
+        @FXML
+        private TextField lastname;
+        @FXML
+        private TextField gebdat;
+        @FXML
+        private TextField email;
+        @FXML
+        private TextField position;
+        @FXML
+        private TextField marktvalue;
+        @FXML
+        private TextField mannschaftid;
+        @FXML
+        private Button confirmbtn;
+        @FXML
+        private Button showbtn;
+        @FXML
+        private Button forwardbtn;
+        @FXML
+        private Button backbtn;
+        @FXML
+        private Button deletebtn;
+        @FXML
+        private TextField natio;
+        @FXML
+        private Button confirmbtn1;
 
+        private boolean isNewEntry = false;
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        myPersonList = PersonDAO.getPersons();
-        listProperty.set(FXCollections.observableArrayList(myPersonList));
+        @Override
+        public void initialize(URL url, ResourceBundle rb) {
+            myPersonList = PersonDAO.getPersons();
+            listProperty.set(FXCollections.observableArrayList(myPersonList));
 
-        currentIndex.addListener((obs, oldIndex, newIndex) -> {
-            if (newIndex.intValue() >= 0 && newIndex.intValue() < myPersonList.size()) {
-                showPersonDetails(myPersonList.get(newIndex.intValue()));
+            currentIndex.addListener((obs, oldIndex, newIndex) -> {
+                if (newIndex.intValue() >= 0 && newIndex.intValue() < myPersonList.size()) {
+                    showPersonDetails(myPersonList.get(newIndex.intValue()));
+                }
+            });
+
+            // Enable/disable navigation buttons based on the current index
+            BooleanBinding isFirstPerson = currentIndex.isEqualTo(0);
+            BooleanBinding isLastPerson = currentIndex.isEqualTo(myPersonList.size() - 1);
+            backbtn.disableProperty().bind(isFirstPerson);
+            forwardbtn.disableProperty().bind(isLastPerson);
+
+            // Zeige die Details der ersten Person an (falls vorhanden)
+            if (!myPersonList.isEmpty()) {
+                currentIndex.set(0);
             }
-        });
-
-        // Enable/disable navigation buttons based on the current index
-        BooleanBinding isFirstPerson = currentIndex.isEqualTo(0);
-        BooleanBinding isLastPerson = currentIndex.isEqualTo(myPersonList.size() - 1);
-        backbtn.disableProperty().bind(isFirstPerson);
-        forwardbtn.disableProperty().bind(isLastPerson);
-
-        // Zeige die Details der ersten Person an (falls vorhanden)
-        if (!myPersonList.isEmpty()) {
-            currentIndex.set(0);
-    }
-}
-    
+        }
 
         @FXML
         private void handleConfirmButton(ActionEvent event) {
+            if (isNewEntry) {
+                // Neuer Datensatz
+                    actPerson = new Person(
+                    0, // spielerId (you can set it to 0 or provide a unique identifier)
+                    firstname.getText(), // vorname
+                    lastname.getText(), // nachname
+                    Integer.parseInt(gebdat.getText()), // geburtsdatum
+                    natio.getText(), // Nationalitaet
+                    email.getText(), // email
+                    position.getText(), // position
+                    Integer.parseInt(marktvalue.getText()), // marktwert
+                    Integer.parseInt(mannschaftid.getText()) // mannschaftId
+                );
+
+                PersonDAO.addPerson(actPerson);
+                myPersonList.add(actPerson);
+                currentIndex.set(myPersonList.size() - 1);
+
+
+                isNewEntry = false;
+            } else {
                 // Aktualisiere die Daten der aktuell ausgewählten Person
                 actPerson.setVorname(firstname.getText());
                 actPerson.setNachname(lastname.getText());
@@ -89,45 +112,58 @@ public class FXMLDocumentController implements Initializable {
 
                 // Speichern der aktualisierten Person in der Datenbank oder Datenquelle
                 PersonDAO.update(actPerson);
-}
-
-    @FXML
-    private void handleShowButton(ActionEvent event) {
-        if (!myPersonList.isEmpty()) {
-            currentIndex.set(0);
+            }
         }
-    }
 
-    @FXML
-    private void handleForwardButton(ActionEvent event) {
-        if (currentIndex.get() < myPersonList.size() - 1) {
-            currentIndex.set(currentIndex.get() + 1);
+        @FXML
+        private void handleShowButton(ActionEvent event) {
+            if (!myPersonList.isEmpty()) {
+                currentIndex.set(0);
+            }
         }
-    }
 
-    @FXML
-    private void handleBackButton(ActionEvent event) {
-        if (currentIndex.get() > 0) {
-            currentIndex.set(currentIndex.get() - 1);
+        @FXML
+        private void handleForwardButton(ActionEvent event) {
+            if (currentIndex.get() < myPersonList.size() - 1) {
+                currentIndex.set(currentIndex.get() + 1);
+            }
         }
+
+        @FXML
+        private void handleBackButton(ActionEvent event) {
+            if (currentIndex.get() > 0) {
+                currentIndex.set(currentIndex.get() - 1);
+            }
+        }
+
+        @FXML
+        private void handleDeleteButton(ActionEvent event) {
+        }
+
+        @FXML
+        private void handleNewButton(ActionEvent event) {
+            isNewEntry = true;
+            firstname.clear();
+            lastname.clear();
+            gebdat.clear();
+            natio.clear();
+            email.clear();
+            position.clear();
+            marktvalue.clear();
+            mannschaftid.clear();
+        }
+
+        private void showPersonDetails(Person person) {
+            firstname.setText(person.getVorname());
+            lastname.setText(person.getNachname());
+            gebdat.setText(String.valueOf(person.getGeburtsdatum()));
+            natio.setText(person.getNationalitaet());
+            email.setText(person.getEmail());
+            position.setText(person.getPosition());
+            marktvalue.setText(String.valueOf(person.getMarktwert()));
+            mannschaftid.setText(String.valueOf(person.getMannschaftId()));
+
+            actPerson = person; // Initialisiere das actPerson-Objekt mit der übergebenen Person
+        }
+
     }
-
-    @FXML
-    private void handleDeleteButton(ActionEvent event) {
-
-    }
-
-
-    private void showPersonDetails(Person person) {
-        firstname.setText(person.getVorname());
-        lastname.setText(person.getNachname());
-        gebdat.setText(String.valueOf(person.getGeburtsdatum()));
-        natio.setText(person.getNationalitaet());
-        email.setText(person.getEmail());
-        position.setText(person.getPosition());
-        marktvalue.setText(String.valueOf(person.getMarktwert()));
-        mannschaftid.setText(String.valueOf(person.getMannschaftId()));
-        
-        actPerson = person; // Initialisiere das actPerson-Objekt mit der übergebenen Person
-    }
-}
--- ControlDB/src/model/PersonDAO.java
+++ ControlDB/src/model/PersonDAO.java
@@ -3,6 +3,7 @@ package model;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import util.DBConnector;
@@ -12,32 +13,32 @@ import util.DML_DAO;
  * DAO-Klasse für Personen.
  */
 public class PersonDAO {
-    
+
     /**
      * Gibt eine Liste aller Personen aus der Datenbank zurück.
-     * 
+     *
      * @return ObservableList von Personen
      */
     public static ObservableList<Person> getPersons() {
         ObservableList<Person> personsList = FXCollections.observableArrayList();
         Connection con = null;
-        
+
         try {
             con = DBConnector.connect();
             String sql = "SELECT * FROM Spieler";
             ResultSet rs = con.createStatement().executeQuery(sql);
-            
+
             while (rs.next()) {
                 personsList.add(new Person(
-                    rs.getInt("SpielerId"),
-                    rs.getString("Vorname"),
-                    rs.getString("Nachname"),
-                    rs.getInt("Geburtsjahr"),
-                    rs.getString("Nationalitaet"),
-                    rs.getString("Email"),
-                    rs.getString("Position"),
-                    rs.getInt("Marktwert"),
-                    rs.getInt("MannschaftId")
+                        rs.getInt("SpielerId"),
+                        rs.getString("Vorname"),
+                        rs.getString("Nachname"),
+                        rs.getInt("Geburtsjahr"),
+                        rs.getString("Nationalitaet"),
+                        rs.getString("Email"),
+                        rs.getString("Position"),
+                        rs.getInt("Marktwert"),
+                        rs.getInt("MannschaftId")
                 ));
             }
         } catch (SQLException ex) {
@@ -51,29 +52,47 @@ public class PersonDAO {
                 }
             }
         }
-        
+
         return personsList;
     }
-    
+
     /**
      * Aktualisiert die Informationen einer Person in der Datenbank.
-     * 
+     *
      * @param actPerson die aktualisierte Person
      */
     public static void update(Person actPerson) {
-        System.out.println("Update method called.");
         String sql = "UPDATE spieler SET "
-            + "Vorname = '" + actPerson.getVorname() + "', "
-            + "Nachname = '" + actPerson.getNachname() + "', "
-            + "Geburtsjahr = " + actPerson.getGeburtsdatum() + ", "
-            + "Nationalitaet = '" + actPerson.getNationalitaet() + "', "
-            + "Email = '" + actPerson.getEmail() + "', "
-            + "Position = '" + actPerson.getPosition() + "', "
-            + "Marktwert = " + actPerson.getMarktwert() + ", "
-            + "MannschaftId = " + actPerson.getMannschaftId() + " "
-            + "WHERE SpielerId = " + actPerson.getSpielerId();
+                + "Vorname = '" + actPerson.getVorname() + "', "
+                + "Nachname = '" + actPerson.getNachname() + "', "
+                + "Geburtsjahr = " + actPerson.getGeburtsdatum() + ", "
+                + "Nationalitaet = '" + actPerson.getNationalitaet() + "', "
+                + "Email = '" + actPerson.getEmail() + "', "
+                + "Position = '" + actPerson.getPosition() + "', "
+                + "Marktwert = " + actPerson.getMarktwert() + ", "
+                + "MannschaftId = " + actPerson.getMannschaftId() + " "
+                + "WHERE SpielerId = " + actPerson.getSpielerId();
+
+        DML_DAO.executeDML(sql);
+    }
+
+    /**
+     * Fügt eine neue Person zur Datenbank hinzu.
+     *
+     * @param newPerson die hinzuzufügende Person
+     */
+    public static void addPerson(Person newPerson) {
+        String sql = "INSERT INTO spieler (Vorname, Nachname, Geburtsjahr, Nationalitaet, Email, Position, Marktwert, MannschaftId) "
+                    + "VALUES ('" 
+                    + newPerson.getVorname() + "', '"
+                    + newPerson.getNachname() + "', " 
+                    + newPerson.getGeburtsdatum() + ", '"
+                    + newPerson.getNationalitaet() + "', '" 
+                    + newPerson.getEmail() + "', '" 
+                    + newPerson.getPosition() + "', "
+                    + newPerson.getMarktwert() + ", " 
+                    + newPerson.getMannschaftId() + ")";
         
-        System.out.println("sql: " + sql);
         DML_DAO.executeDML(sql);
     }
 }
